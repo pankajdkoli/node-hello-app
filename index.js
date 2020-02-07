3cb5760f609b6ab1811fc1687efe63b661341fad
pankajdkoli <pankajdkoli@gmail.com>
2020-02-07 12:41:12 +0530

    added test endpoint

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ app.get('/', function (req, res) {
   res.send('Hello World asdfasdfada dfasdfasfas')
 })
 
+app.get('/test', function(req, res) {
+    res.send({name: 'pankaj'});
+});
+
 app.listen(4000);
